6f1cdaa556ca6381439776470056fe8b01d61e75
BenStrawbridge <lute_midnight@yahoo.com>
2020-11-06 12:54:47 -0600

    Encapsulation Exercise

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount acct = new BankAccount();
+            Console.WriteLine($"Your starting balance is: {acct.GetBalance()}");
+            acct.Deposit(55);
+            Console.WriteLine($"Your new balance is: {acct.GetBalance()}");
+        }
+    }
+}
